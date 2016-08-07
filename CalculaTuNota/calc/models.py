0a02fabfacac474b80009bc8106ef13e1e46fe3e
Alejandro <alejo8a_2@hotmail.com>
2016-08-07 11:54:28 -0500

    Creada las tablas usuario y notas

--- CalculaTuNota/calc/models.py
+++ CalculaTuNota/calc/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class User(models.Model):
+    username = models.CharField(max_length=64, unique=True, primary_key=True)
+    password = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.username
+
+@python_2_unicode_compatible
+class Grade(models.Model):
+    username   = models.ForeignKey(User, on_delete=models.CASCADE)
+    grade      = models.FloatField()
+    percentage = models.FloatField()
+
+    def __str__(self):
+        return username +" " + grade
